d5fdf1bff805d0faa7bdbb25fefbdbd225a81379
Weberttz <johngaldencio18@gmail.com>
2026-08-25 23:20:21 -0300

    feat: add questao03.c

---
+++ Lista01/questao03.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void f(int v[]) {
+    printf("Dentro de f:\n");
+    printf("sizeof(v)=%zu\n", sizeof(v));
+
+    printf("Quando o veror é passado como parâmetro de uma função, na verdade é passado o endereço de memória");
+    printf("do início do vetor");
+
+    printf("Isso se chama deacimento de vetor");
+
+    printf("O sizeof dentro de f é o tamanho que o ponteiro ocupa ao apontar para a cabeça do vetor v");
+
+    getchar();
+    system("clear");
+
+    printf("STACK: v[0] -> v[9] + *p -> &v\n");
+}
+
+int main() {
+    int v[10];
+
+    printf("No main:\n");
+    printf("sizeof(v)=%zu\n", sizeof(v));
+
+    printf("Como o tamanho do vetor é 10, e cada int ocupa 4 bytes, então o tamanho inteiro que o vetor ocupa é: 40");
+    f(v);
+
+    printf("Os valores são diferentes, pois, na main é feito o calculo da quantidade de espaços em memória que o vetor ocupa\n");
+    printf("Na função f, é realizado o cálculo da quantidade de espaços de memória que o ponteiro que aponta para o inicio");
+    printf("do vetor ocupa");
+    
+
+    return 0;
+}
