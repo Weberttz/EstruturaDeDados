562f84f37eefb6151ff134b6de77662fd27ef54c
Weberttz <johngaldencio18@gmail.com>
2026-08-25 23:41:18 -0300

    refactor: altera linhas de comunicação do arquivo

--- Lista01/questao03.c
+++ Lista01/questao03.c
@@ -2,13 +2,13 @@
 #include <stdlib.h>
 
 void f(int v[]) {
-    printf("Dentro de f:\n");
-    printf("sizeof(v)=%zu\n", sizeof(v));
+    printf("\nDentro de f: ");
+    printf("sizeof(v)= %zu\n", sizeof(v));
 
     printf("Quando o veror é passado como parâmetro de uma função, na verdade é passado o endereço de memória");
     printf("do início do vetor");
 
-    printf("Isso se chama deacimento de vetor");
+    printf("Isso se chama deacimento de vetor\n");
 
     printf("O sizeof dentro de f é o tamanho que o ponteiro ocupa ao apontar para a cabeça do vetor v");
 
@@ -21,8 +21,8 @@ void f(int v[]) {
 int main() {
     int v[10];
 
-    printf("No main:\n");
-    printf("sizeof(v)=%zu\n", sizeof(v));
+    printf("No main: ");
+    printf("sizeof(v)= %zu\n", sizeof(v));
 
     printf("Como o tamanho do vetor é 10, e cada int ocupa 4 bytes, então o tamanho inteiro que o vetor ocupa é: 40");
     f(v);
