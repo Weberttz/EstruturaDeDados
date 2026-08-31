fb6bb7081675eab1d863abcfa7c3613eac23540a
Weberttz <johngaldencio18@gmail.com>
2026-08-31 14:41:31 -0300

    refactor: modifica questao07.c

--- Lista01/questao07.c
+++ Lista01/questao07.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
+#include <string.h>
 
 void inicializa(int *v, int n) {
-
     int *p = v;
-    while (p <= v + n) {
+    while (p < v + n) {
         *p = 0;
         p++;
     }
@@ -17,10 +17,13 @@ int main(){
 
     printf("A função inicializa(int *v, int n) zera todos os elementos do vetor\n");
 
-    for(int i=0; i<4; i++)
-        printf("%d", v[i]);
+    for(int i=0; i<4; i++){
+        printf("%d ", v[i]);
+    }
 
-    printf("O erro do código era pela falta de função main");
+    printf("\nO erro do código é por conta da igualdade no while\n");
+    printf("Existe um erro na linha 6\n");
+    printf("O erro ocorre por acessar área adjacente fora do escopo do vetor, isso causa problemas se zerar ");
 
     return 0;
 }
